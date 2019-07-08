5670f81f00bdc4226e8d6e32c5e383ace20e5a24
Lukas Herrmann <Lukas@L-MacBook.local>
2019-07-08 22:17:24 +0200

    upload of rest

--- 04_conditionals_loops/03_05_sum.py
+++ 04_conditionals_loops/03_05_sum.py
@@ -5,3 +5,10 @@ Take two numbers from the user, an upper and lower bound. Using a loop, calculat
 	For example, if a user enters 1 and 100, the output should be:
 		The sum is: 5050
 '''
+
+input1 = int(input("Type a lower bound number: "))
+input2 = int(input("Type a upper bound number: "))
+sum = 0
+for i in range(input1, input2+1):
+    sum += i
+print("The sum is: ", sum)
--- 04_conditionals_loops/03_07_search.py
+++ 04_conditionals_loops/03_07_search.py
@@ -4,3 +4,11 @@ Receive a number between 0 and 1,000,000,000 from the user.
 Use while loop to find the number - when the number is found exit the loop and print the number to the console.
 
 '''
+
+input1 = int(input("Type in number between 0 and 1000000000: "))
+num = 1
+while num in range(1,1000000000):
+        if num == input1:
+            print(num)
+        num +=1
+
