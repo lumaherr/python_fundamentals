0377f4c989d3701043c1e61b061c1ecd1c155229
Lukas Herrmann <Lukas@L-MacBook.local>
2019-07-09 22:02:55 +0200

    05_02_rps.py had difficulties

--- 05_functions/05_01_tasks.py
+++ 05_functions/05_01_tasks.py
@@ -14,3 +14,21 @@ Write a script that completes the following tasks.
 # calls a function that determines whether the number is divisible by 4 or 7 exclusively
 
 # print the results
+
+input1 = int(input("Write number between 1 and 1000000000: "))
+if input1 % 4 == 0 and input1 % 7 == 0:
+    print("True")
+else: print(False)
+if input1 % 4 ==0 or input1 % 7 == 0:
+    print("True")
+else: print(False)
+
+
+# dont know the last bit
+"""
+if input1 % 4 ==0 or input1 % 7 == 0:
+    for i in (range(2,input1-1) if x != 4 and x !=7):
+        if input1 % i == 1:
+            print("True")
+else: print(False)
+"""
--- 05_functions/05_02_rps.py
+++ 05_functions/05_02_rps.py
@@ -12,15 +12,64 @@ def get_hand(hand):
     # for example if the variable hand is 0, it should return the string "scissor"
     pass
 
+
 # function should take in two hands and return a string "You won!" or "You lost :(" or "You tied!"
 def determine_winner(computer, player):
     pass
 
 
-
 '''
 Start here
+
 '''
+
+input_user = int(input("Type in a number between 0 and 2 (0 = scissor, 1 = rock, 2 = paper): "))
+
+import random
+
+input_mac = random.randint(0, 2)
+
+# i dont know how to write this correct
+
+def get_hand(n):
+    if n == 0:
+        print("scissor")
+    elif n == 1:
+        print("rock")
+    elif n == 2:
+        print("paper")
+    else:
+        print("error, user typed in not wanted number")
+    return get_hand()
+
+
+a = get_hand(input_user)
+b = get_hand(input_mac)
+print(a)
+print(b)
+
+if a == b:
+    print("You tied!")
+elif a == "scissor":
+    if b == "rock":
+        print("You lost :(")
+elif a == "rock":
+    if b == "scissor":
+        print("You won!")
+elif a == "paper":
+    if b == "rock":
+        print("You won!")
+elif a == "rock":
+    if b == "paper":
+        print("You lost :(")
+elif a == "scissor":
+    if b == "paper":
+        print("You won!")
+elif a == "paper":
+    if b == "scissor":
+        print("You lost :(")
+
+
 
 # take in a number 0-2 from the user that represents their hand
 
