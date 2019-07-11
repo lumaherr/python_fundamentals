903e97763a293912f7b8362e33afa72f5f22d178
Lukas Herrmann <Lukas@L-MacBook.local>
2019-07-11 22:25:01 +0200

    upload of the first 2 tasks

--- 06_classes_objects_methods/06_01_car.py
+++ 06_classes_objects_methods/06_01_car.py
@@ -9,3 +9,18 @@ Create at least two different objects of this Car class and demonstrate
 changing the objects attributes.
 
 '''
+
+class Car:
+    def __init__(self, model, year, max_speed):
+        self.model = model
+        self.year = year
+        self.max_speed = max_speed
+
+    def turbo(self):
+        self.max_speed += 5
+
+    def __str__(self):
+       return f"The car {self.model} from {self.year} has a maximum speed of {self.max_speed}."
+
+a = Car("Porsche", 2019, 200)
+print(a)
--- 06_classes_objects_methods/06_02_shapes.py
+++ 06_classes_objects_methods/06_02_shapes.py
@@ -7,3 +7,21 @@ Write methods in the appropriate class so that you can calculate the area (of th
 perimeter (of the rectangle) and circumference of the circle.
 
 '''
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.lenght = length
+        self.width = width
+    def __str__(self):
+        return f"The area is {self.lenght * self.width} and the perimeter is {self.lenght * 2 + self.width * 2}"
+
+import math
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def __str__(self):
+        return f"The area is {self.radius * math.pi ** 2} and the circumefence is {(self.radius * 2) * math.pi}"
+a = Rectangle(2, 2)
+b = Circle(2)
+print(a)
+print(b)
