23bb2cd9de51cbc86f1451361ae0a84a54ce787b
Lukas Herrmann <Lukas@L-MacBook.local>
2019-07-12 13:31:27 +0200

    adding rest

--- 06_classes_objects_methods/06_03_freeform.py
+++ 06_classes_objects_methods/06_03_freeform.py
@@ -8,13 +8,50 @@
 - Create a print method in each class that prints out the attributes
     in a nicely formatted string.
 - Include a __str__ method in each class.
+
 - Overload the __add__ method in one of the classes so that it's possible
     to add attributes of two instances of that class using the + operator.
+
 - Once the objects are created, change some of the attribute values.
 
 Be creative. Have some fun. :)
 Using objects you can model anything you want.
 Cars, animals, poker games, sports teams, trees, beers, people etc...
-
-
 '''
+
+class Car:
+    def __init__(self, model, amount, PS):
+        self.model = model
+        self.amount = amount
+        self.PS = PS
+    def __str__(self):
+        return f"{self.model} is a car which is {self.amount} times in stock with {self.PS} PS"
+    def Turbo(self):
+        self.PS += 5
+    def aging(self):
+        self.PS -= 10
+
+
+class Motorbike:
+    def __init__(self, model, amount, PS):
+        self.model = model
+        self.amount = amount
+        self.PS = PS
+    def __str__(self):
+        return f"{self.model} is a Motorbike which is {self.amount} times in stock with {self.PS} PS"
+
+class Quad:
+    def __init__(self, model, amount, PS):
+        self.model = model
+        self.amount = amount
+        self.PS = PS
+    def __str__(self):
+        return f"{self.model} is a Quad which is {self.amount} times in stock with {self.PS} PS"
+
+
+a = Quad("Yamaha", 5, 100)
+b = Car("Porsche", 2, 300)
+print(a)
+print(b)
+b.Turbo()
+print(b)
--- 06_classes_objects_methods/06_04_inheritance.py
+++ 06_classes_objects_methods/06_04_inheritance.py
@@ -16,3 +16,51 @@ your own for this exercise but if you are stuck, some ideas include:
 - A Restaurant superclass, with Gourmet and FastFood subclasses.
 
 '''
+
+class Car:
+    def __init__(self, model, amount, PS):
+        self.model = model
+        self.amount = amount
+        self.PS = PS
+    def __str__(self):
+        return f"{self.model} is a car which is {self.amount} times in stock with {self.PS} PS"
+    def Turbo(self):
+        self.PS += 5
+    def aging(self):
+        self.PS -= 10
+
+class Supercar(Car):
+    def supercar1(self):
+        print("this car is very fast")
+
+class oldtimer(Car):
+    def old(self):
+        print("this car is very old")
+
+class Motorbike:
+    def __init__(self, model, amount, PS):
+        self.model = model
+        self.amount = amount
+        self.PS = PS
+    def __str__(self):
+        return f"{self.model} is a Motorbike which is {self.amount} times in stock with {self.PS} PS"
+
+class Quad:
+    def __init__(self, model, amount, PS):
+        self.model = model
+        self.amount = amount
+        self.PS = PS
+    def __str__(self):
+        return f"{self.model} is a Quad which is {self.amount} times in stock with {self.PS} PS"
+
+
+a = Quad("Yamaha", 5, 100)
+b = Car("Porsche", 2, 300)
+c = oldtimer("Porsche", 2, 300)
+d = Supercar("Porsche", 2, 300)
+print(a)
+print(b)
+b.Turbo()
+print(b)
+c.old()
+d.supercar1()
