dfe6edf28d2a579803e28c75acddbb04c3842d81
Lukas Herrmann <Lukas@L-MacBook.local>
2019-07-18 18:24:47 +0200

    still working

--- 13_aggregate_functions/13_01_enumerate.py
+++ 13_aggregate_functions/13_01_enumerate.py
@@ -1,3 +1,8 @@
 '''
 Demonstrate the use of the .enumerate() function.
 '''
+
+my_list = ["hi", "my", "name", "is", "Lukas", "and", "I am", "22", "years", "old" ]
+
+for i, c in enumerate(my_list, 1):
+    print(f"The word: {c}, is number: {i} in the list ")
--- 13_aggregate_functions/13_02_sum.py
+++ 13_aggregate_functions/13_02_sum.py
@@ -2,3 +2,11 @@
 Write a simple aggregate function, sum(), that takes a list a returns the sum.
 
 '''
+
+my_list = [1, 3, 5, 6, 10]
+for i in range(20, 30):
+    my_list.append(i)
+
+print(my_list)
+my_list_sum = sum(my_list)
+print(my_list_sum)
