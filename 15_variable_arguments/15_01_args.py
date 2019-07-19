61a9bb6a86d5646e01f5a3b11e795deb68ebacfa
Lukas Herrmann <Lukas@L-MacBook.local>
2019-07-19 14:32:33 +0200

    new upload

--- 15_variable_arguments/15_01_args.py
+++ 15_variable_arguments/15_01_args.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def hello_state(*germany):
+    for i in germany:
+        print(i)
+
+hello_state("NRW", "Bavaria", "Berlin", "Bremen", "Hamburg")
--- 15_variable_arguments/15_02_kwargs.py
+++ 15_variable_arguments/15_02_kwargs.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def cars(**PS):
+    for a,b in PS.items():
+        print(a,b)
+
+cars(Porsche = "500", Maserati = "600", Audi = "200")
