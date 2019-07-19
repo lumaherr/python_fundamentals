a952a942e5d7e84ce6c48b5196a5494bd1af7ae8
Lukas Herrmann <Lukas@L-MacBook.local>
2019-07-19 12:29:42 +0200

    upload

--- 17_generators/17_01_generators.py
+++ 17_generators/17_01_generators.py
@@ -3,3 +3,7 @@ Demonstrate how to create a generator object. Print the object to the console to
 Then iterate over the generator object and print out each item.
 
 '''
+gen = (i for i in range(1,500) if i%3 == 0 and i%5 != 0)
+
+for i in gen:
+    print(i)
