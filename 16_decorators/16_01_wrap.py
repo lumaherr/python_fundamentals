65730991da2fb747775df85b94e09dbcb7d05c04
Lukas Herrmann <Lukas@L-MacBook.local>
2019-07-22 15:17:51 +0200

    upload

--- 16_decorators/16_01_wrap.py
+++ 16_decorators/16_01_wrap.py
@@ -3,3 +3,22 @@ Write a decorator function that wraps text passed to it in a specified html tag.
 The user should be able to decide which tag to use.
 
 '''
+
+def decorator_func(initial_func):
+    def wrapper_func():
+        print("The user requests the tag: ")
+        return initial_func()
+
+    return wrapper_func
+
+@decorator_func
+def paragraph_1():
+    print("<p>")
+
+@decorator_func
+def header_1():
+    print("<h1>")
+
+@decorator_func
+def link_1():
+    print("<a>")
