e765dbbdac74f4ef981b498b17153a372ce95275
Lukas Herrmann <Lukas@L-MacBook.local>
2019-07-22 16:09:59 +0200

    upload

--- 18_lambdas/18_01_first_lambda.py
+++ 18_lambdas/18_01_first_lambda.py
@@ -2,3 +2,7 @@
 Write a lambda function that takes in 3 numbers and returns the sum of the numbers.
 
 '''
+
+sum_1 = lambda x, y, z,: x + y + z
+
+print(sum_1(1,2,3))
--- 18_lambdas/18_02_return.py
+++ 18_lambdas/18_02_return.py
@@ -2,3 +2,6 @@
 Write a lambda function that does not take in an arguments but returns a value. Print the return value.
 
 '''
+
+value = lambda : 5
+print(value())
